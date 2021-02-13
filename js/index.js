6dd97214b08e13768f38e2c6c8709cef9847bdeb
wbuddi <wbuddi@gmail.com>
2021-02-13 13:15:07 +1030

    BUTTONS

---
+++ js/index.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Chess is a HOBBY!!";
+}
+
+function myFunction2() {
+    document.getElementById("demo2").innerHTML = "WHY DID YOU CLICK?  Chess is a HOBBY!!";
+}
